dcc5fca98420f77cd47a9a7d344b8db1a54c2a26
Andrew So <acso@cpp.edu>
2019-02-18 21:52:17 -0800

    Create log0.php

---
+++ logs/log0.php
@@ -0,0 +1,42 @@
+<p>Meeting at: Library</p>
+<p>Meeting Objective: Figuring what the project is all about. Getting to know each other.</p>
+<table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th>Finished Tasks</th>
+    <th>New Tasks</th>
+    <th>Attendance</th>
+  </tr>
+  <tr>
+    <td>Chloe Stabinsky</td>
+    <td>-</td>
+    <td>-</td>
+    <td>Yes</td>
+  </tr>
+  <tr>
+    <td>Andrew So</td>
+    <td>-</td>
+    <td>-</td>
+    <td>Yes</td>
+  </tr>
+  <tr>
+    <td>Dylan Chung</td>
+    <td>-</td>
+    <td>-</td>
+    <td>Yes</td>
+  </tr>
+  <tr>
+    <td>Nowndale Sale</td>
+    <td>-</td>
+    <td>-</td>
+    <td>Yes</td>
+  </tr>
+  <tr>
+    <td>Mohammed Khan</td>
+    <td>-</td>
+    <td>-</td>
+    <td>Wasn't in group yet</td>
+  </tr>
+</table>
+
+<p>Notes: None </p>
