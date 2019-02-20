f8e74bb41ff8ac96329ecec9c34c7a3bf94ea3a2
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-02-19 20:33:46 -0800

    Fix contacts page and typos. Update emails.

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
-<h2>Contacts</h2>
+<h2>Contact Us</h2>
 <p>Chloe Stabinsky: <a href="mailto:cmstabinsky@cpp.edu">cmstabinsky@cpp.edu</a></p>
-<p>Andrew So:</p>
-<p>Dylan Young:</p>
-<p>Nowndale Sale:</p>
+<p>Andrew So: <a href="mailto:acso@cpp.edu">acso@cpp.edu</a></p>
+<p>Dylan Chung: <a href="mailto:dychung@cpp.edu">dychung@cpp.edu</a></p>
+<p>Nowndale Sale: <a href="mailto:nesale@cpp.edu">nesale@cpp.edu</a></p>
 <p>Mohammed Khan: <a href="mailto:mohammedk@cpp.edu">mohammedk@cpp.edu</a></p>
