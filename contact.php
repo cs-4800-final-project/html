4f5c1cc06df1fc9b54fd00fc035984c5769d2f23
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-02-19 20:27:37 -0800

    Update contacts page.

--- contact.php
+++ contact.php
@@ -1,1 +1,6 @@
-Contact Page under Construction.
+<h2>Contacts</h2>
+<p>Chloe Stabinsky: <a href="mailto:cmstabinsky@cpp.edu">cmstabinsky@cpp.edu</a></p>
+<p>Andrew So:</p>
+<p>Dylan Young:</p>
+<p>Nowndale Sale:</p>
+<p>Mohammed Khan: <a href="mailto:mohammedk@cpp.edu">mohammedk@cpp.edu</a></p>
