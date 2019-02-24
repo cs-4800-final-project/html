6b8409cb79e26132020c7aa845b5d10061a44539
Andrew So <acso@cpp.edu>
2019-02-24 13:37:32 -0800

    update menubar

--- header.php
+++ header.php
@@ -7,4 +7,9 @@
 	<a href="index.php?page=project">Project</a>
 	<a href="index.php?page=member">Members</a>
 	<a href="index.php?page=contact">Contact Us</a>
+	// Check if the user is logged in, if not then redirect him to login page
+	if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== false){
+    
+    	<a href="logout.php">Logout</a>
+	}
 </div>
