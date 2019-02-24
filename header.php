19b99da3745bfc119a4c0fa5c131a89b1f05a99f
Andrew So <acso@cpp.edu>
2019-02-24 13:49:37 -0800

    update header again

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+session_start();
 <div id="menubar">
 	<a href="index.php">
   		<img src="assets/nufu.png" alt="Home" style="width:42px;height:42px;border:0">
@@ -7,14 +8,19 @@
 	<a href="index.php?page=project">Project</a>
 	<a href="index.php?page=member">Members</a>
 	<a href="index.php?page=contact">Contact Us</a>
-	<?php if( isset($_SESSION['username']) && !empty($_SESSION['username']) )
+	<?php 
+	if( isset($_SESSION['username']) && !empty($_SESSION['username']) )
 	{
 		?>
 	      <a href="logout.php">Logout</a>
 		<?php 
-	}else{ ?>
-     <a href="login.php">Login</a>
-     <a href="register.php">Register</a>
-	<?php 
-	} ?>
+	}
+	else
+	{ 
+		?>
+     	<a href="login.php">Login</a>
+     	<a href="register.php">Register</a>
+		<?php 
+	} 	
+	?>
 </div>
