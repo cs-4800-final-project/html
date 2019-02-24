400411fb15e51e49521dc6044a6fde24f36943e5
Andrew So <acso@cpp.edu>
2019-02-24 13:07:29 -0800

    login-database commit

--- home.php
+++ home.php
@@ -1,8 +1,15 @@
-<<<<<<< HEAD
+
 <h2>Welcome to our Homepage.</h2>
 This will be changed after initial update.
-=======
-Welcome to out Homepage.
-This will be changed after initial update.
 <a href="https://github.com/cs-4800-final-project">Github Project</a>
->>>>>>> c64c0a3eb8af8ff0fb1e0fbe88c9b529ce70a225
+
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
