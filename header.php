ceb0a361cea4bce5c4d0b7e3d2ab9ccadbece551
Andrew So <acso@cpp.edu>
2019-02-24 13:46:52 -0800

    update header

--- header.php
+++ header.php
@@ -7,9 +7,14 @@
 	<a href="index.php?page=project">Project</a>
 	<a href="index.php?page=member">Members</a>
 	<a href="index.php?page=contact">Contact Us</a>
-	// Check if the user is logged in, if not then redirect him to login page
-	if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== false){
-    
-    	<a href="logout.php">Logout</a>
-	}
+	<?php if( isset($_SESSION['username']) && !empty($_SESSION['username']) )
+	{
+		?>
+	      <a href="logout.php">Logout</a>
+		<?php 
+	}else{ ?>
+     <a href="login.php">Login</a>
+     <a href="register.php">Register</a>
+	<?php 
+	} ?>
 </div>
