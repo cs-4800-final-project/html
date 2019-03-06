80ed41dd4d29a8d7fc37c7031c30bfb44772d975
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-03-05 20:52:58 -0800

    Add log 12. Fix typos for log 11.

---
+++ logs/day12.php
@@ -0,0 +1,42 @@
+<p>Meeting at: Voice chat</p>
+<p>Meeting Objective: Continue working on project</p>
+<table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th>Finished Tasks</th>
+    <th>New Tasks</th>
+    <th>Attendance</th>
+  </tr>
+  <tr>
+    <td>Chloe Stabinsky</td>
+    <td>Look into Socket.io </td>
+    <td>Research Socket.io more </td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Andrew So</td>
+    <td>Work on middleware. </td>
+    <td>Clean root access. Continue working on middleware.</td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Dylan Chung</td>
+    <td>Work on auto-play for video feature. </td>
+    <td>Finish making footer</td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Nowndale Sale</td>
+    <td>Look into video streaming. </td>
+    <td>Research streaming. </td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Mohammed Khan</td>
+    <td> </td>
+    <td> </td>
+    <td>No </td>
+  </tr>
+</table>
+
+<p>Notes: Finish database info, middleware, and landing page.</p>
