a208cc876305f4fb48b6525584865a286fcde920
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-03-11 10:24:35 -0700

    Add log 16.

---
+++ logs/day16.php
@@ -0,0 +1,42 @@
+<p>Meeting at: In class</p>
+<p>Meeting Objective: Figure out what we need to do</p>
+<table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th>Finished Tasks</th>
+    <th>New Tasks</th>
+    <th>Attendance</th>
+  </tr>
+  <tr>
+    <td>Chloe Stabinsky</td>
+    <td>Learn that we're probably using comments section. </td>
+    <td>Look into comments for html. </td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Andrew So</td>
+    <td>Separate company website with product website. </td>
+    <td>Finish middleware sometime this year. Get video on website. </td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Dylan Chung</td>
+    <td>Finish video page. </td>
+    <td>Go back to profile page.  </td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Nowndale Sale</td>
+    <td> </td>
+    <td> </td>
+    <td>No </td>
+  </tr>
+  <tr>
+    <td>Mohammed Khan</td>
+    <td>Start changing php to vue.js. </td>
+    <td>Figure how to implement strips & styles. </td>
+    <td>Yes </td>
+  </tr>
+</table>
+
+<p>Notes: GET VIDEO DONE. cAT MEMES FTW</p>
