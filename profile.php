7194a19cd313928bb753bda3511d5fd5dd471ed8
Sameer <sameer095k@gmail.com>
2019-03-19 22:14:56 -0700

    organized profile page

--- profile.php
+++ profile.php
@@ -1,28 +1,14 @@
 <h2>Profile Page</h2>
-<h4>User Name</h4>
-<p>Sameer097k</p>
+<h4>User Info</h4>
+<p>User Name: Sameer097k</p>
+<p>Email: Sameer095k@gmail.com</p>
+<p>Phone Number: 123-456-7890</p>
+<p>Address: 123456 NuFu Dr. Pomona CA 90765</p>
 
-
-<h4>Shows Subscribed To</h4>
-<p>Show #1: The Office</p>
-<p>Show #2: Daredevil</p>
-<p>Show #3: Friends</p>
-
-<p>Show #10: You</p>
-
-
-<h4>Address</h4>
-<p>123456 NuFu Dr. Pomona CA 90765</p>
-
-<h4>Payment Method</h4>
+<h4>Payment Info</h4>
 <p>Type: Debit</p>
 <p>*******9999</p>
 
-<h4>Email</h4>
-<p>Sameer095k@gmail.com</p>
-
-<h4>Phone Number</h4>
-<p>123-456-7890</p>
-
-<h4>Days Left in Subscription</h4>
-<p>30</p>
+<h4>Subscription Ifno</h4>
+<p>Days Left in Subscription: 30</p>
+<p>Shows Subscribed To: The Office, Daredevil, Friends, Your Name</p>
