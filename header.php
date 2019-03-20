027f8468463446ee1569991dd76999dfcb8206f1
Sameer <sameer095k@gmail.com>
2019-03-19 21:52:35 -0700

    profile page changes 3

--- header.php
+++ header.php
@@ -17,8 +17,7 @@
 			</li>
 				<a class ="nav-link" href = "http://danc.ddns.net/index.php?page=contact"> Contact Us</a>
 			</li>
-			</li>
-				<a class ="nav-link" href = "http://danc.ddns.net/index.php?page=profile"> Contact Us</a>
+				<a class ="nav-link" href = "http://danc.ddns.net/index.php?page=profile"> Profile</a>
 			</li>
 			<?php 
 			session_start();
