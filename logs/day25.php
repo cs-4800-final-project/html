45f34952a0b60c89164f60a5c53b55795e69fd62
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-03-22 10:40:16 -0700

    Populate log 25.

--- logs/day25.php
+++ logs/day25.php
@@ -1,5 +1,5 @@
 <p>Meeting at: In class</p>
-<p>Meeting Objective: </p>
+<p>Meeting Objective: Start working on subscription thingy.</p>
 <table style="width:100%">
   <tr>
     <th>Name</th>
@@ -9,34 +9,34 @@
   </tr>
   <tr>
     <td>Chloe Stabinsky</td>
-    <td> </td>
-    <td> </td>
-    <td> </td>
+    <td>Clean logos and code.</td>
+    <td>Continue that. </td>
+    <td>Yes </td>
   </tr>
   <tr>
     <td>Andrew So</td>
-    <td> </td>
-    <td> </td>
-    <td> </td>
+    <td>Finish search bar. </td>
+    <td>Overhaul project website for cleaning. </td>
+    <td>Yes </td>
   </tr>
   <tr>
     <td>Dylan Chung</td>
-    <td> </td>
-    <td> </td>
-    <td> </td>
+    <td>Adding mute button. </td>
+    <td>Get video gallery to play videos when hover mouse. </td>
+    <td>Yes </td>
   </tr>
   <tr>
     <td>Nowndale Sale</td>
-    <td> </td>
-    <td> </td>
-    <td> </td>
+    <td>Reserached how to  comments on video page. </td>
+    <td>Put comments on video page. </td>
+    <td>Yes </td>
   </tr>
   <tr>
     <td>Mohammed Khan</td>
     <td> </td>
     <td> </td>
-    <td> </td>
+    <td>No </td>
   </tr>
 </table>
 
-<p>Notes: </p>
+<p>Notes: jklddjklfdsajklfdsajlkfjdslk</p>
