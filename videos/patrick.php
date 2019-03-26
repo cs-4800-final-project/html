fa2f026c70bf6fa82f3599b4291d93a861d4fbe2
nowndale <47119630+nowndale@users.noreply.github.com>
2019-03-26 09:28:37 -0700

    Update patrick.php

--- videos/patrick.php
+++ videos/patrick.php
@@ -84,9 +84,10 @@
   <div class="form-group">
   	<label for="comment">Comment:</label>
   	<textarea class="form-control" rows="5" id="comment"></textarea>
+	<input type="submit" class="btn btn-info" value="Submit">
   </div> 
 	
-  <input type="submit" class="btn btn-info" value="Submit">
+ 
 
 <?php
 	
