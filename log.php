9b66695019fcbea26c5760cf6020345f8bc41e11
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-03-26 20:33:31 -0700

    Add log 27.

--- log.php
+++ log.php
@@ -1,4 +1,10 @@
 <h2>Week 6: Mar 25 - Mar 29</h2>
+<button class="collapsible">Log 27: Mar 26, 2019</button>
+<div class="content">
+  <?php
+    include('logs/day27.php');
+  ?>
+</div>
 <button class="collapsible">Log 26: Mar 25, 2019</button>
 <div class="content">
   <?php
