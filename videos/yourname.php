0b7c52c6e7876a03e7570e8738aafcd462665f98
nowndale <47119630+nowndale@users.noreply.github.com>
2019-04-10 09:41:23 -0700

    Update yourname.php

--- videos/yourname.php
+++ videos/yourname.php
@@ -10,31 +10,31 @@
 
 <?php
 	echo  '<nav class ="navbar navbar-expand-md navbar-light bg-light sticky-top">
-<div class = "container-fluid"> 
-	<a class = "navbar-brand" href="#"><img src = "../img/logo.png"> </a>
-	<button class="navbar-toggler" type= "button" data-toggle="collapse" data-target = "#navbarResponsive">
-		<span class =  "navbar-toggler-icon"></span>
-	</button>
+		<div class = "container-fluid"> 
+			<a class = "navbar-brand" href="#"><img src = "../img/logo.png"> </a>
+				<button class="navbar-toggler" type= "button" data-toggle="collapse" data-target = "#navbarResponsive">
+					<span class =  "navbar-toggler-icon"></span>
+				</button>
 	<!--<?php echo $_POST['search']; ?>-->
-	<form action="http://danc.ddns.net/search/?ID=" method="POST">
+		<form action="http://danc.ddns.net/search/?ID=" method="POST">
 			<input type="text" name="search" placeholder="Search">
 			<button type="submit" name="submit-search">Search</button>
-	</form>
-	<div class = "collapse navbar-collapse" id= "navbarResponsive">
-		<ul class = "navbar-nav ml-auto">
-			<li class = "nav-item">
-				<a class ="nav-link" href = "http://danc.ddns.net/home/"> Home </a>
-			</li>
-				<a class ="nav-link" href = "http://danc.ddns.net/profile/profile.php"> Profile</a>
-			</li>
+		</form>
+		<div class = "collapse navbar-collapse" id= "navbarResponsive">
+			<ul class = "navbar-nav ml-auto">
+				<li class = "nav-item">
+					<a class ="nav-link" href = "http://danc.ddns.net/home/"> Home </a>
+				</li>
+					<a class ="nav-link" href = "http://danc.ddns.net/profile/profile.php"> Profile</a>
+				</li>
 			<?php 
-			session_start();
-			if( isset($_SESSION['username']) && !empty($_SESSION['username']) )
-			{
-				?>
-			      <a class ="nav-link"href="../dbcon/logout.php">Logout</a>
+				session_start();
+				if( isset($_SESSION['username']) && !empty($_SESSION['username']) )
+				{
+			?>
+			<a class ="nav-link"href="../dbcon/logout.php">Logout</a>
 			      </li>
-				<?php 
+			<?php 
 			}
 			else
 			{ 
