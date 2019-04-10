6472a861258b7812a8a969ffe098c532c574779a
Sameer <sameer095k@gmail.com>
2019-04-09 22:37:53 -0700

    try search redirect

--- header.php
+++ header.php
@@ -4,8 +4,8 @@
 	<button class="navbar-toggler" type= "button" data-toggle="collapse" data-target = "#navbarResponsive">
 		<span class =  "navbar-toggler-icon"></span>
 	</button>
-
-	<form action="http://danc.ddns.net/search/?ID=<?php echo $_POST['search']; ?>" method="POST">
+	<!--<?php echo $_POST['search']; ?>-->
+	<form action="http://danc.ddns.net/search/?ID=" method="POST">
 			<input type="text" name="search" placeholder="Search">
 			<button type="submit" name="submit-search">Search</button>
 	</form>
