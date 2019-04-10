6ea2a625baad5f72aedb527c02478b4d24bf1ee8
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-04-09 21:31:15 -0700

    Add log 33

---
+++ depreciated/logs/day33.php
@@ -0,0 +1,42 @@
+<p>Meeting at: Discord</p>
+<p>Meeting Objective: Finish up editable profile and fix webpage issues. Start working on following and subscriptions.</p>
+<table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th>Finished Tasks</th>
+    <th>New Tasks</th>
+    <th>Attendance</th>
+  </tr>
+  <tr>
+    <td>Chloe Stabinsky</td>
+    <td>Research into databases for following others. </td>
+    <td>Look into changing the login page to looking better? </td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Andrew So</td>
+    <td>Finish GUI for changing profile info. </td>
+    <td>Implement search filters. </td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Dylan Chung</td>
+    <td>Make comment section UI. </td>
+    <td>Make it better.</td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Nowndale Sale</td>
+    <td> </td>
+    <td> </td>
+    <td>No </td>
+  </tr>
+  <tr>
+    <td>Mohammed Khan</td>
+    <td>Working on search bar. </td>
+    <td>Continue that. </td>
+    <td>Yes </td>
+  </tr>
+</table>
+
+<p>Notes: Need to do subscriptions, following, serach bar, dynamic profile page, etc.</p>
