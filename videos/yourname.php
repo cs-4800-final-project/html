ea3e87490fd9ff105025bea6c31d6e049d77b798
nowndale <47119630+nowndale@users.noreply.github.com>
2019-04-10 09:46:14 -0700

    Update yourname.php

--- videos/yourname.php
+++ videos/yourname.php
@@ -8,16 +8,56 @@
 <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 <link rel="stylesheet" href="assets/style.css">
 
-
+<?php
+	echo  '<nav class ="navbar navbar-expand-md navbar-light bg-light">
+<div class = "container-fluid"> 
+	<a class = "navbar-brand" href="#"><img src = "img/logo.png"> </a>
+	<button class="navbar-toggler" type= "button" data-toggle="collapse" data-target = "#navbarResponsive">
+		<span class =  "navbar-toggler-icon"></span>
+	</button>
+	<div class = "collapse navbar-collapse" id= "navbarResponsive">
+		<ul class = "navbar-nav ml-auto">
+			<li class = "nav-item">
+				<a class ="nav-link" href = "http://danc.ddns.net"> Home </a>
+			</li>
+				<a class ="nav-link" href = "http://danc.ddns.net/index.php?page=log"> Logs </a>
+			</li>
+				<a class ="nav-link" href = "http://danc.ddns.net/index.php?page=srs"> SRS </a>
+			</li>
+				<a class ="nav-link" href = "http://danc.ddns.net/index.php?page=project"> Database </a>
+			</li>
+			</li>
+				<a class ="nav-link" href = "http://danc.ddns.net/index.php?page=er"> ER Model </a>
+			</li>
+				<a class ="nav-link" href = "http://danc.ddns.net/index.php?page=contact"> Contact Us</a>
+			</li>
+			<?php 
+			session_start();
+			if( isset($_SESSION[\'username\']) && !empty($_SESSION[\'username\']) )
+			{
+				?>
+			      <a class ="nav-link"href="logout.php">Logout</a>
+			      </li>
+				<?php 
+			}
+			else
+			{ 
+				?>
+		     	<a class ="nav-link"href="login.php">Login</a>
+		     	</li>
+		     	<a class ="nav-link"href="register.php">Register</a>
+		     	</li>
+				<?php 
+			} 	
+			?>
+		</ul>
+	</div>
+</div>
+</nav>
+';?>
 
 <?php
 	session_start();
-
-	// Check if the user is already logged in, if false then redirect him to welcome page
-	if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === !true){
-  		  header("location: ../home/");
-    	exit;
-	}
 	
 	echo '<div style="position:relative; padding-bottom:56.25%; overflow:hidden;">
 	<iframe src="https://cdn.jwplayer.com/players/kGD375IS-wMbjKtxS.html" width="100%" height="100%" frameborder="0" 
@@ -112,7 +152,6 @@
 	
 	include('footer.php')
 ?>
-
 
 
 
