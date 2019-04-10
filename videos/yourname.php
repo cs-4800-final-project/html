dcbc8203a6853db5e604001929d90bf8f3632e06
nowndale <47119630+nowndale@users.noreply.github.com>
2019-04-10 09:36:55 -0700

    Update yourname.php

--- videos/yourname.php
+++ videos/yourname.php
@@ -9,43 +9,39 @@
 <link rel="stylesheet" href="assets/style.css">
 
 <?php
-	echo  '<nav class ="navbar navbar-expand-md navbar-light bg-light">
+	echo  '<nav class ="navbar navbar-expand-md navbar-light bg-light sticky-top">
 <div class = "container-fluid"> 
-	<a class = "navbar-brand" href="#"><img src = "img/logo.png"> </a>
+	<a class = "navbar-brand" href="#"><img src = "../img/logo.png"> </a>
 	<button class="navbar-toggler" type= "button" data-toggle="collapse" data-target = "#navbarResponsive">
 		<span class =  "navbar-toggler-icon"></span>
 	</button>
+	<!--<?php echo $_POST['search']; ?>-->
+	<form action="http://danc.ddns.net/search/?ID=" method="POST">
+			<input type="text" name="search" placeholder="Search">
+			<button type="submit" name="submit-search">Search</button>
+	</form>
 	<div class = "collapse navbar-collapse" id= "navbarResponsive">
 		<ul class = "navbar-nav ml-auto">
 			<li class = "nav-item">
-				<a class ="nav-link" href = "http://danc.ddns.net"> Home </a>
+				<a class ="nav-link" href = "http://danc.ddns.net/home/"> Home </a>
 			</li>
-				<a class ="nav-link" href = "http://danc.ddns.net/index.php?page=log"> Logs </a>
-			</li>
-				<a class ="nav-link" href = "http://danc.ddns.net/index.php?page=srs"> SRS </a>
-			</li>
-				<a class ="nav-link" href = "http://danc.ddns.net/index.php?page=project"> Database </a>
-			</li>
-			</li>
-				<a class ="nav-link" href = "http://danc.ddns.net/index.php?page=er"> ER Model </a>
-			</li>
-				<a class ="nav-link" href = "http://danc.ddns.net/index.php?page=contact"> Contact Us</a>
+				<a class ="nav-link" href = "http://danc.ddns.net/profile/profile.php"> Profile</a>
 			</li>
 			<?php 
 			session_start();
-			if( isset($_SESSION[\'username\']) && !empty($_SESSION[\'username\']) )
+			if( isset($_SESSION['username']) && !empty($_SESSION['username']) )
 			{
 				?>
-			      <a class ="nav-link"href="logout.php">Logout</a>
+			      <a class ="nav-link"href="../dbcon/logout.php">Logout</a>
 			      </li>
 				<?php 
 			}
 			else
 			{ 
 				?>
-		     	<a class ="nav-link"href="login.php">Login</a>
+		     	<a class ="nav-link"href="../login/login.php">Login</a>
 		     	</li>
-		     	<a class ="nav-link"href="register.php">Register</a>
+		     	<a class ="nav-link"href="../register/register.php">Register</a>
 		     	</li>
 				<?php 
 			} 	
@@ -53,8 +49,7 @@
 		</ul>
 	</div>
 </div>
-</nav>
-';?>
+</nav>';?>
 
 <?php
 	session_start();
