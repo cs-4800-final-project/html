aa1eae830dbf0076f5e31a88dec66e858b15900c
nowndale <47119630+nowndale@users.noreply.github.com>
2019-04-10 09:32:54 -0700

    Update yourname.php

--- videos/yourname.php
+++ videos/yourname.php
@@ -60,7 +60,7 @@
 	session_start();
 
 	// Check if the user is already logged in, if yes then redirect him to welcome page
-	if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+	if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === false){
   		  header("location: ../home/");
     	exit;
 	}
