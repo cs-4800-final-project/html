eb2ae8b597843a06a8d248bbc2d93b155508306e
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-04-17 11:13:41 -0700

    Add log 39

---
+++ depreciated/logs/day39.php
@@ -0,0 +1,42 @@
+<p>Meeting at: In Class</p>
+<p>Meeting Objective: Fix getting hacked.</p>
+<table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th>Finished Tasks</th>
+    <th>New Tasks</th>
+    <th>Attendance</th>
+  </tr>
+  <tr>
+    <td>Chloe Stabinsky</td>
+    <td>Work on description.</td>
+    <td>Continue that.</td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Andrew So</td>
+    <td>Reset database. Deal with shit.</td>
+    <td>Reconstruct database.</td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Dylan Chung</td>
+    <td>Make friend page.</td>
+    <td>Wish and watch list. </td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Nowndale Sale</td>
+    <td>Added videos </td>
+    <td>Rating system. </td>
+    <td>Yes </td>
+  </tr>
+  <tr>
+    <td>Mohammed Khan</td>
+    <td>NPopulate database. Make search work. </td>
+    <td>Make dynamic vid page. Help with database. </td>
+    <td>Yes </td>
+  </tr>
+</table>
+
+<p>Notes: </p>
