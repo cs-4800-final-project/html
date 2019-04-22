dce90e0db0fbe40bb3c7652c6cd1cfbf18957c0b
Sameer <sameer095k@gmail.com>
2019-04-22 11:44:31 -0700

    changes to footer.php

--- header.php
+++ header.php
@@ -1,3 +1,63 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-gray fixed-top ">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#"> <img src="../img/logo.png"> </h1> </a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+				<span class="navbar-toggler-icon"> <span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+				<ul class="navbar-nav ml-auto">
+
+			<?php
+			if ( isset($_POST['submit-search']) )
+			{
+				$searchq = $_POST['search'];
+				$baseurl = "http://danc.ddns.net/search/?ID=";
+				$completeurl = $baseurl.$searchq;
+				header( "Location:".$completeurl ); 
+			}
+			?>
+
+			<form class="form-inline" method="POST">
+			<div class="md-form my-0">
+				<input type="text" class="form-control form-control-sm mr-3 w-80" name="search" placeholder="Search">
+				<button type="submit" name="submit-search">Search</button>
+			</div>
+			</form>
+
+
+					<!--
+
+					<form class="form-inline ">
+												<div class="md-form my-0">
+							<input class="form-control form-control-sm mr-3 w-80" type="text" placeholder="Search"
+								aria-label="Search">
+							<i class="fas fa-search" aria-hidden="true"></i>
+						</div>
+					</form>
+					-->
+
+					<li class="nav-item">
+						<a class="nav-link" href="../home"> Home </a>
+					</li>
+					<a class="nav-link" href=""> My Subscriptions </a>
+					</li>
+					<a class="nav-link" href=""> Shows </a>
+					</li>
+					<a class="nav-link" href="#"> Movies </a>
+					</li>
+					<a class="nav-link" href="http://danc.ddns.net/profile"> My Profile </a>
+					<!--Make this a dropdown w/ profile picture later -->
+					</li>
+					<a class="nav-link" href="http://danc.ddns.net/dbcon/logout.php"> Logout </a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+
+
+<!-- old header
 <nav class ="navbar navbar-expand-md navbar-light bg-light sticky-top">
 <div class = "container-fluid"> 
 	<a class = "navbar-brand" href="#"><img src = "../img/logo.png"> </a>
@@ -5,7 +65,6 @@
 		<span class =  "navbar-toggler-icon"></span>
 	</button>
 
-<!-- this is the search part-->
 	<?php
 	if ( isset($_POST['submit-search']) )
 	{
@@ -52,3 +111,5 @@
 	</div>
 </div>
 </nav>
+
+-->
