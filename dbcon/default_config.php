d54ab86150adbb170eeba245697ff7d8ca0c8ba3
Andrew So <asoso221@gmail.com>
2019-04-22 05:47:07 -0700

    security check

--- dbcon/default_config.php
+++ dbcon/default_config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'webBot');
-define('DB_PASSWORD', 'broncorule');
-define('DB_NAME', 'information');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
