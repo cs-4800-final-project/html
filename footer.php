4ac29e7135c713880625978aedddbcf8884984dd
nikurou <47164155+nikurou@users.noreply.github.com>
2019-04-30 13:01:58 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,36 @@
 <div class="sticky-bottom" style="float:right; width:100%; padding-top:5%;">
  <footer id="footer">
-      <p>Student Project: Software Engineering Cal Poly Pomona (C) 2019</p>
-      <p>Thanks for visiting NuFu</p>
+                <div class="wrap">
+
+                <div class="left-footer">
+                    <h3>About Us</h3>
+                    <h3>FAQ</h3>
+                    <h3>Contact Us</h3>
+                    <h3>Terms & Conditions</h3>
+                </div>
+
+
+                <div class="right-footer">
+
+                    <h3> Follow us on Social Media!</h3>
+
+                    <h2> 
+                     <i class="fab fa-facebook-square"></i>
+                       <i class="fab fa-twitter-square"></i> 
+                       <i class="fab fa-google-plus-square"></i>
+
+
+                    </h2>
+
+                </div>
+                <div class="center-footer">
+                        <h3> DISCLAIMER <h3>
+                        <h3>NUFU is an academic Project and not a real service</h3>
+                        <h3>NUFU does not have real social media pages</h3>
+                        <h3>NUFU is not a real company, do not contact us</h3>
+             
+                </div>
+
+            </div>
  </footer>
 </div>
