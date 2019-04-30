239bb39000b21727cf9ddcc402df35cd423a8494
Sameer <sameer095k@gmail.com>
2019-04-30 12:36:38 -0700

    fixes

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div class="sticky-bottom" style="padding-top:150%;">
+<div class="sticky-bottom" style="float:right; width:100%; padding-top:5%;">
  <footer id="footer">
       <p>Student Project: Software Engineering Cal Poly Pomona (C) 2019</p>
       <p>Thanks for visiting NuFu</p>
