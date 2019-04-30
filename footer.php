b9fe9aa7e300d2ae7429f0b2ad73efc1c45c0cfb
Sameer <sameer095k@gmail.com>
2019-04-30 12:35:40 -0700

    fixes

--- footer.php
+++ footer.php
@@ -1,40 +1,6 @@
-<!--Old Footer for in case this doesn't work-->
-<!--<div id="footer">(C)2019 - Student Semester Project </div>-->
-
-<!--- Footer -->
-<div class = "sticky-bottom" style = "padding-top:300%;">
-	<footer id = "footer">
-		<div class="wrap">
-
-				<div class="left-footer">
-					<h3><a href="#">About Us</a></h3>
-					<h3><a href="#">FAQ</a></h3>
-					<h3><a href="#">Contact Us</a></h3>
-					<h3><a href="#">Terms & Conditions</a></h3>
-				</div>
-			
-				
-				<div class="right-footer">
-
-						<h3> Follow us on Social Media!</h3>
-
-						<h2> <a href="#"><i class="fab fa-facebook-square"></i></a>
-							<a href="#"><i class="fab fa-twitter-square"></i> </a>
-							<a href="#"><i class="fab fa-google-plus-square"></i> </a>
-						
-						
-						</h2>
-						
-				</div>
-				
-				<div class="center-footer">
-						<h3><a href="#">About Us</a></h3>
-						<h3><a href="#">FAQ</a></h3>
-						<h3><a href="#">Contact Us</a></h3>
-						<h3><a href="#">Terms & Conditions</a></h3>
-				</div>
-				
-				</div>
-				
-	</footer>
+<div class="sticky-bottom" style="padding-top:150%;">
+ <footer id="footer">
+      <p>Student Project: Software Engineering Cal Poly Pomona (C) 2019</p>
+      <p>Thanks for visiting NuFu</p>
+ </footer>
 </div>
