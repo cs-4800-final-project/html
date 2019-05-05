5ba8e88403b2655e7c346ae9b8a934f8d89a8b7b
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-05-04 19:39:58 -0700

    Clean code and try to fix footer

--- footer.php
+++ footer.php
@@ -1,34 +1,12 @@
 <div class="sticky-bottom" style="float:right; width:100%; padding-top:5%;">
  <footer id="footer">
                 <div class="wrap">
-
-                <div class="left-footer">
-                    <h3>About Us</h3>
-                    <h3>FAQ</h3>
-                    <h3>Contact Us</h3>
-                    <h3>Terms & Conditions</h3>
-                </div>
-
-
-                <div class="right-footer">
-
-                    <h3> Follow us on Social Media!</h3>
-
-                    <h2> 
-                     <i class="fab fa-facebook-square"></i>
-                       <i class="fab fa-twitter-square"></i> 
-                       <i class="fab fa-google-plus-square"></i>
-
-
-                    </h2>
-
-                </div>
                 <div class="center-footer">
                         <h3> DISCLAIMER <h3>
                         <h3>NUFU is an academic Project and not a real service</h3>
                         <h3>NUFU does not have real social media pages</h3>
                         <h3>NUFU is not a real company, do not contact us</h3>
-             
+
                 </div>
 
             </div>
