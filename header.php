106a73f9afa97f24fa826339b4ba5bfab0f5433b
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-05-04 19:17:36 -0700

    Add links for See More option in profile

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-gray fixed-top ">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="#"> <img src="http://danc.ddns.net/img/logo.png"> </h1> </a>
+			<a class="navbar-brand" href="http://danc.ddns.net/home/"> <img src="http://danc.ddns.net/img/logo.png"> </h1> </a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
 				<span class="navbar-toggler-icon"> <span>
 			</button>
@@ -13,7 +13,7 @@
 				$searchq = $_POST['search'];
 				$baseurl = "http://danc.ddns.net/search/?ID=";
 				$completeurl = $baseurl.$searchq;
-				header( "Location:".$completeurl ); 
+				header( "Location:".$completeurl );
 			}
 			?>
 
@@ -59,7 +59,7 @@
 
 <!-- old header
 <nav class ="navbar navbar-expand-md navbar-light bg-light sticky-top">
-<div class = "container-fluid"> 
+<div class = "container-fluid">
 	<a class = "navbar-brand" href="#"><img src = "../img/logo.png"> </a>
 	<button class="navbar-toggler" type= "button" data-toggle="collapse" data-target = "#navbarResponsive">
 		<span class =  "navbar-toggler-icon"></span>
@@ -71,7 +71,7 @@
 		$searchq = $_POST['search'];
 		$baseurl = "http://danc.ddns.net/search/?ID=";
 		$completeurl = $baseurl.$searchq;
-		header( "Location:".$completeurl ); 
+		header( "Location:".$completeurl );
 	}
 	?>
 
@@ -88,24 +88,24 @@
 			</li>
 				<a class ="nav-link" href = "http://danc.ddns.net/profile/profile.php"> Profile</a>
 			</li>
-			<?php 
+			<?php
 			session_start();
 			if( isset($_SESSION['username']) && !empty($_SESSION['username']) )
 			{
 				?>
 			      <a class ="nav-link"href="../dbcon/logout.php">Logout</a>
 			      </li>
-				<?php 
+				<?php
 			}
 			else
-			{ 
+			{
 				?>
 		     	<a class ="nav-link"href="../login/login.php">Login</a>
 		     	</li>
 		     	<a class ="nav-link"href="../register/register.php">Register</a>
 		     	</li>
-				<?php 
-			} 	
+				<?php
+			}
 			?>
 		</ul>
 	</div>
