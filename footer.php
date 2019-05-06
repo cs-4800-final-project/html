2e911fec5a313b153184376346e8a864ec7b0dbf
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-05-05 20:43:45 -0700

    Try to get footer to work. :U

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
-<div class="footer-dark">
-    <footer>
-      <div class="center-footer">
-              <h3> DISCLAIMER <h3>
-              <h3>NUFU is an academic Project and not a real service. Please do not contact us.</h3>
+<footer>
+  <div class="footer-dark">
+      <footer>
+        <div class="center-footer">
+                <h3> DISCLAIMER <h3>
+                <h3>NUFU is an academic Project and not a real service. Please do not contact us.</h3>
 
-      </div>
-    </footer>
-</div>
+        </div>
+  </div>
+</footer>
