545f9e0d241bc62b87c795e490f22a12f2247e94
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-05-05 20:09:58 -0700

    Still messing with footers. Might look like trash for a bit. Sorry.

--- footer.php
+++ footer.php
@@ -1,13 +1,9 @@
-<div class="sticky-bottom" style="float:right; width:100%; padding-top:5%;">
- <footer id="footer">
-                <div class="wrap">
-                <div class="center-footer">
-                        <h3> DISCLAIMER <h3>
-                        <h3>NUFU is an academic Project and not a real service</h3>
-                        <h3>NUFU does not have real social media pages</h3>
-                        <h3>NUFU is not a real company, do not contact us</h3>
-                </div>
+<div class="footer-dark">
+    <footer>
+      <div class="center-footer">
+              <h3> DISCLAIMER <h3>
+              <h3>NUFU is an academic Project and not a real service. Please do not contact us.</h3>
 
-            </div>
- </footer>
+      </div>
+    </footer>
 </div>
