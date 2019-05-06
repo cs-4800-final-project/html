079aaea082ef569c9e1a4cd2ccd3faeffea6c665
Chloe Stabinsky <chloestabinsky@gmail.com>
2019-05-05 21:08:36 -0700

    Try a different footer

---
+++ footer_inside.php
@@ -0,0 +1,10 @@
+<div class="sticky-bottom" style="float:right; width:100%; padding-top:5%;">
+ <footer id="footer">
+                <div class="wrap">
+                <div class="center-footer">
+                        <h3> DISCLAIMER <h3>
+                        <h3>NUFU is an academic Project and not a real service. Please do not contact us.</h3>
+                </div>
+            </div>
+ </footer>
+</div>
